d39d733240ba5d652320c512adfc9286e6326ce1
Isaku Yamahata <yamahata@valinux.co.jp>
2011-08-31 13:17:26 +0900

    fullmesh topology with hosts
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>

--- examples/fullmesh.py
+++ examples/fullmesh.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
 """
-This example creates a multi-controller network from
+This example creates a multi-controller fullmesh network from
 semi-scratch; note a topo object could also be used and
 would be passed into the Mininet() constructor.
 """
 
 from mininet.net import Mininet
-from mininet.node import Controller, OVSKernelSwitch
+from mininet.node import Controller, OVSKernelSwitch, RemoteController
 from mininet.cli import CLI
-from mininet.log import setLogLevel
+from mininet.log import setLogLevel, debug
 
 Switch = OVSKernelSwitch
 
@@ -19,39 +19,57 @@ def addHost( net, N ):
     ip = '10.0.0.%d' % N
     return net.addHost( name, ip=ip )
 
-def multiControllerNet():
+controller_ip = '172.16.3.33'   # XXX
+
+def fullMeshMultiControllerNet(nswitches, nhosts_per_switch, ncontrollers):
     "Create a network with multiple controllers."
+    debug("nswtiches %d hosts/switch %d controllers %d\n" %
+          (nswitches, nhosts_per_switch, ncontrollers))
 
     net = Mininet( controller=Controller, switch=Switch)
 
     print "*** Creating controllers"
-    c1 = net.addController( 'c1', port=6633 )
-    c2 = net.addController( 'c2', port=6634 )
+    clist = []
+    port = 6633
+    for c in range(0, ncontrollers):
+        debug("controller %d\n" % c)
+        clist.append(net.addController(name='c%d' % (c + 1),
+                                       controller=RemoteController,
+                                       ip=controller_ip,
+                                       port=port))
+        port += 1
 
     print "*** Creating switches"
-    s1 = net.addSwitch( 's1' )
-    s2 = net.addSwitch( 's2' )
+    slist = []
+    for s in range(0, nswitches):
+        controller = s * ncontrollers / nswitches
+        s_n = s + 1
+        debug("switch %d controller %d\n" % (s_n, controller))
+        switch = net.addSwitch('s%d' % s_n,
+                               controller = controller)
+        for other in slist:
+            other.linkTo(switch)
+        slist.append(switch)
 
     print "*** Creating hosts"
-    hosts1 = [ addHost( net, n ) for n in 3, 4 ]
-    hosts2 = [ addHost( net, n ) for n in 5, 6 ]
-
-    print "*** Creating links"
-    for h in hosts1:
-        s1.linkTo( h )
-    for h in hosts2:
-        s2.linkTo( h )
-    s1.linkTo( s2 )
+    hlists = []
+    for s in range(0, nswitches):
+        hlist = []
+        for h in range(0, nhosts_per_switch):
+            h_n = s * nhosts_per_switch + h + 1
+            host = addHost(net, h_n)
+            hlist.append(host)
+            slist[s].linkTo(host)
+        hlists.append(hlist)
 
     print "*** Starting network"
-    net.build()
-    c1.start()
-    c2.start()
-    s1.start( [ c1 ] )
-    s2.start( [ c2 ] )
+    net.start()
 
-    print "*** Testing network"
-    net.pingAll()
+    # print "*** wait for nox to be stable"
+    # time.sleep(30)
+
+    # print "*** Testing network"
+    # net.pingAll()
 
     print "*** Running CLI"
     CLI( net )
@@ -59,6 +77,10 @@ def multiControllerNet():
     print "*** Stopping network"
     net.stop()
 
+
 if __name__ == '__main__':
-    setLogLevel( 'info' )  # for CLI output
-    multiControllerNet()
+    #setLogLevel( 'info' )  # for CLI output
+    setLogLevel( 'debug' )  # for CLI output
+    fullMeshMultiControllerNet(5, 1, 2)
+    #fullMeshMultiControllerNet(5, 1, 1)
+    #fullMeshMultiControllerNet(3, 1, 1)
