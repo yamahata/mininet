f421b888c423b62aab5719eecef630b6c0cefa12
Bob Lantz <rlantz@cs.stanford.edu>
2012-06-15 17:15:28 -0700

    Change to install install-precise branch

--- util/vm/install-mininet-vm.sh
+++ util/vm/install-mininet-vm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script is intended to install Mininet into
-# a brand-new Ubuntu (10.04 or 11.10) virtual machine,
+# a brand-new Ubuntu 12.04 virtual machine,
 # to create a fully usable "tutorial" VM.
 
 set -e
@@ -20,8 +20,8 @@ sudo apt-get update
 sudo apt-get -y install git-core openssh-server
 git clone git://github.com/mininet/mininet
 cd mininet
-# Currently ovs-1.4-compat; will change to testing or master
-git checkout -b 1.4 origin/devel/ovs-1.4-compat
+# Currently install-precise; will change to testing or master
+git checkout -b 1.4 origin/devel/install-precise
 cd
 time mininet/util/install.sh
 if ! grep NOX_CORE_DIR .bashrc; then
